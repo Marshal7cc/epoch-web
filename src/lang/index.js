3925d6cb8aac08568063373612ae0339698e3e96
Marshal <5loveme>
2020-01-28 00:03:29 +0800

    [add]-设置element ui 内部组件多语言

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+
+// 设置element内部UI Local,例如分页
+import ElementUILocale from 'element-ui/lib/locale'
+
 import elementEnLocale from 'element-ui/lib/locale/lang/en'
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'
 import elementJaLocale from 'element-ui/lib/locale/lang/ja'
@@ -7,6 +11,7 @@ import elementJaLocale from 'element-ui/lib/locale/lang/ja'
 import enLocale from './en'
 import zhLocale from './zh'
 import jaLocale from './ja'
+
 import cache from '@/utils/cache'
 
 Vue.use(VueI18n)
@@ -25,6 +30,7 @@ const messages = {
     ...elementJaLocale
   }
 }
+
 export function getLanguage() {
   const chooseLanguage = cache.get('lang', '')
   if (chooseLanguage) return chooseLanguage
@@ -39,6 +45,7 @@ export function getLanguage() {
   }
   return 'zh'
 }
+
 const i18n = new VueI18n({
   // set locale
   // options: en | zh | es
@@ -48,4 +55,24 @@ const i18n = new VueI18n({
   silentTranslationWarn: true
 })
 
+// 设置elementUI内部组件语言
+
+ElementUILocale.use(getElementUILocales(getLanguage()))
+
+const ElementUILocales = {
+  zh: elementZhLocale,
+  en: elementEnLocale,
+  ja: elementJaLocale
+}
+
+const getElementUILocales = lang => {
+  switch (lang) {
+    case 'zh':
+      return ElementUILocales.zh
+    case 'en':
+      return ElementUILocales.en
+    case 'ja':
+      return ElementUILocales.ja
+  }
+}
 export default i18n
--- src/lang/zh.js
+++ src/lang/zh.js
@@ -1,7 +1,5 @@
 const epoch = JSON.parse(localStorage.getItem('prompt'))
 
-debugger
-
 export default {
   epoch
 }
